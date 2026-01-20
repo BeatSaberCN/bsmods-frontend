d11b476e02726faffea9451900a5c5ac21637b64
frto027 <q602706150@gmail.com>
2026-01-20 20:25:37 +0800

    footer

--- src/lib/components/ui/layout/Footer.svelte
+++ src/lib/components/ui/layout/Footer.svelte
@@ -7,7 +7,6 @@
     <div class="ml-5 flex flex-row items-center gap-4">
       <p class="my-auto w-max text-xs">本项目是BeatMods项目的下游中文项目! -></p>
       <div class="flex h-fit flex-col gap-1 sm:flex-row sm:gap-4">
-
         <a
           class="my-auto !h-fit !w-fit text-xs duration-150 hover:text-brand-foreground-link-hover"
           href="https://github.com/BeatSaberCN/bsmods-frontend"
@@ -15,29 +14,21 @@
           rel="no-referrer"
           >前端（源码）</a
         >
+
         <a
           class="my-auto !h-fit !w-fit text-xs duration-150 hover:text-brand-foreground-link-hover"
           href="https://github.com/BeatSaberCN/beatmods-agent"
           target="=_blank"
           rel="no-referrer"
-            >中文本地化后端</a
-        >
-        <a
-          class="my-auto !h-fit !w-fit text-xs duration-150 hover:text-brand-foreground-link-hover"
-          href="https://github.com/Futuremappermydud/bsmods-frontend"
-          target="=_blank"
-          rel="no-referrer"
-          >前端（上游源码）</a
+            >中文后端（源码）</a
         >
         <a
           class="my-auto !h-fit !w-fit text-xs duration-150 hover:text-brand-foreground-link-hover"
           href="https://beatmods.com"
           target="=_blank"
           rel="no-referrer"
-          >beatmods.com(上游)</a
+          >beatmods.com(上游,api)</a
         >
-
-        API请参考BeatMods网站。
       </div>
     </div>
   </div>
