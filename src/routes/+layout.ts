0cadb98c97e448c1a83c67ab00def293fdb610e1
frto027 <q602706150@gmail.com>
2026-01-20 13:26:03 +0800

    remove auth

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -12,51 +12,13 @@ export const load: LayoutLoad = async () => {
     userId: -1,
     roles: null,
   };
-
-  try {
-    let response = await axios
-      .get(appendURL("api/auth"), {
-        withCredentials: true,
-      })
-      .then((response) => {
-        if (response.status === 302 || response.status === 200) {
-          if (response.data !== null) {
-            return response;
-          }
-        } else {
-          return null;
-        }
-      })
-      .catch((error) => {
-        return null;
-      });
-    if (!response) {
-      userData = {
-        hasAttempted: true,
-        authenticated: false,
-        username: "Guest",
-        userId: -1,
-        roles: null,
-      };
-      return userData;
-    }
-    let data = response.data;
-    userData = {
-      hasAttempted: true,
-      authenticated: true,
-      username: data.user.username,
-      userId: data.user.id,
-      roles: data.user.roles,
-    };
-  } catch (error) {
-    userData = {
-      hasAttempted: true,
-      authenticated: false,
-      username: "Guest",
-      userId: -1,
-      roles: null,
-    };
-  }
+  userData = {
+    hasAttempted: true,
+    authenticated: false,
+    username: "Guest",
+    userId: -1,
+    roles: null,
+  };
 
   return userData;
 };
